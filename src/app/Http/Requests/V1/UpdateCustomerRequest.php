db30d9f20fd8dead003747efcb6e99c452af2ff1
kamil19862307 <kamil19862307@gmail.com>
2026-01-17 17:49:10 +0300

    feat(customers): update customer with put and patch

--- src/app/Http/Requests/V1/UpdateCustomerRequest.php
+++ src/app/Http/Requests/V1/UpdateCustomerRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\V1;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateCustomerRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class UpdateCustomerRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -21,8 +22,44 @@ class UpdateCustomerRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
-            //
-        ];
+        $method = $this->method();
+
+
+        if ($method == 'PUT') {
+
+            return [
+                'name' => 'required',
+                'email' => 'required|email',
+                'type' => ['required', Rule::in('I', 'B', 'i', 'b')],
+                'address' => 'required',
+                'city' => 'required',
+                'state' => 'required',
+                'postal_code' => 'required',
+            ];
+
+        } else {
+
+            return [
+                'name' => 'sometimes|required',
+                'email' => 'sometimes|required|email',
+                'type' => ['sometimes', 'required', Rule::in('I', 'B', 'i', 'b')],
+                'address' => 'sometimes|required',
+                'city' => 'sometimes|required',
+                'state' => 'sometimes|required',
+                'postal_code' => 'sometimes|required',
+            ];
+        }
+    }
+
+    // Данные будут приходить из json в CamelCase, а в базе snake_case, поэтому изменим postalCode на postal_code
+    protected function prepareForValidation()
+    {
+        if ($this->postalCode) {
+
+            $this->merge([
+                'postal_code' => $this->postalCode,
+            ]);
+
+        }
     }
 }
