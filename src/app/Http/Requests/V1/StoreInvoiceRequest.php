46e89232a63981661845cf1fe763c737b3e51018
kamil19862307 <kamil19862307@gmail.com>
2026-01-22 08:00:16 +0300

    feat(sanctum): check authorize rights in requests

--- src/app/Http/Requests/V1/StoreInvoiceRequest.php
+++ src/app/Http/Requests/V1/StoreInvoiceRequest.php
@@ -11,7 +11,9 @@ class StoreInvoiceRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        $user = $this->user();
+
+        return $user !== null && $user->tokenCan('create');
     }
 
     /**
